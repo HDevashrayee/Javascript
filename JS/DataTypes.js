e234a9afe9ff5a1d19d3a84f31d4294072a8f4b4
Hitesh Devashrayee <hitesh@consperate.com>
2022-10-06 10:22:40 +0530

    Commit

---
+++ JS/DataTypes.js
@@ -0,0 +1,17 @@
+//Number: Floting point number used for decimals and integers.
+let number=23;
+
+
+//String:
+let fName='Hitesh';
+let lNamw="Devashrayee";
+
+//Boolean
+let isAvailable=true;
+
+
+//Undefined: value taken by variable that is not defined.
+//Null: 'Empty value'
+//Symbol(ES2015): Value that is unique and can not be changed.
+//Bigint(ES2020):Larger integer than the Number type can hold.
+
---
+++ Script.js
@@ -0,0 +1,3 @@
+console.log("Hello JS");
+let fName= "Hitesh";
+console.log(fName);
