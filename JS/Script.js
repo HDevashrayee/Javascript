5cc9ddd85cd650f39d0c1bcc0531dc0686ec4f40
HDevashrayee <hitesh.devashrayee@outlook.com>
2022-11-13 10:17:47 +0530

    Commit

--- JS/Script.js
+++ JS/Script.js
@@ -4,14 +4,24 @@
 let fName= "Hitesh";
 console.log(fName); */
 
+//Variable declaration.
+
+let score = 0;
+
 //Handling click events.
 
 document.querySelector('.btnscore').addEventListener('click', function(){
-    let score = document.querySelector('.score').value;
+    score = document.querySelector('.score').value;
 
     document.querySelector('.result').value=score;
+    ManupilateStyle();
 });
 
-/* document.querySelector('.score').value=9;
-let score = document.querySelector('.score').value;
-document.querySelector('.result').value = score; */
+// Manupilate CSS 
+
+function ManupilateStyle(){
+if(score>0)
+{
+document.querySelector('body').style.backgroundColor = '#6B728E';
+}
+}
